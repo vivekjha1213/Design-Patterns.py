348009996a5d247f7a4383e8cdac2560e51adafe
vivek jha <vivekjha1213@gmail.com>
2024-01-28 11:16:49 +0530

    Added singleton-design-pattern

--- design_patterns/singleton.py
+++ design_patterns/singleton.py
@@ -1,5 +1,28 @@
-
+from typing import Any
 
 class MetaClass(type):
-    """this is singleton design pattern."""
+    """This is singleton design pattern."""
+    
+    _instance = {}
+    
+    def __call__(cls, *args: Any, **kwds: Any) -> Any:
+        """if instance already exits dont create.."""
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass,cls).__call__(*args, **kwds)
+            
+            return cls._instance[cls]
+        
+class A(metaclass = MetaClass ):
+    
+    def __init__(self) -> None:
+        pass
+
+
+
+#creating a obj
+
+a = A()
+
+print(f"Creating object :{a}")
+    
     
